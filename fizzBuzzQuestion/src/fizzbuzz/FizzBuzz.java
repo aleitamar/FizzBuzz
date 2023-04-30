82ed4a9f009e93e477b9e6f324e6a75cf24999be
ebenharu <ebenharu@bmc.com>
2023-04-30 15:58:10 +0300

    Amend description

--- fizzBuzzQuestion/src/fizzbuzz/FizzBuzz.java
+++ fizzBuzzQuestion/src/fizzbuzz/FizzBuzz.java
@@ -2,11 +2,11 @@ package fizzbuzz;
 
 public class FizzBuzz {
 	/** 
-	 * If the number is a multiple of 3, say “Fizz”
-	 * If it is a multiple of 5, say “Buzz”
-	 * If it is a multiple of 3 and 5, say “FizzBuzz”
-	 * Otherwise, just say the number.
-	 * @param number
+	 * If the number is a multiple of 3, return “Fizz”
+	 * If it is a multiple of 5, return “Buzz”
+	 * If it is a multiple of 3 and 5, return “FizzBuzz”
+	 * Otherwise, just return the number.
+	 * @param number - the input number
 	 * @return a FizzBuzz string
 	 */
 	public String of(int number) {
