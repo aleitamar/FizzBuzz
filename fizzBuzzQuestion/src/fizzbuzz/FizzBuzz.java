a11686091dd063e6f9ef22efca99ebdfdc90cbaa
ebenharu <ebenharu@bmc.com>
2025-11-25 08:59:33 +0200

    Change strange quotes

--- fizzBuzzQuestion/src/fizzbuzz/FizzBuzz.java
+++ fizzBuzzQuestion/src/fizzbuzz/FizzBuzz.java
@@ -2,9 +2,9 @@ package fizzbuzz;
 
 public class FizzBuzz {
 	/** 
-	 * If the number is a multiple of 3, return “Fizz”
-	 * If it is a multiple of 5, return “Buzz”
-	 * If it is a multiple of 3 and 5, return “FizzBuzz”
+	 * If the number is a multiple of 3, return "Fizz"
+	 * If it is a multiple of 5, return "Buzz"
+	 * If it is a multiple of 3 and 5, return "FizzBuzz"
 	 * Otherwise, just return the number.
 	 * @param number - the input number
 	 * @return a FizzBuzz string
